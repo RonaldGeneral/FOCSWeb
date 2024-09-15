8758b64682599d3cada81c345419305d5d3ad1cd
RonaldGeneral <gogo23993@gmail.com>
2024-09-15 16:52:21 +0800

    Setup node

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
