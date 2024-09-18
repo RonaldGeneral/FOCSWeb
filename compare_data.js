ae4c40357705ea4de9065961905439ba115c7542
SoulSpectre <cwongjh3@gmail.com>
2024-09-18 10:10:33 +0800

    compare_programme, compare_data, compare.js

---
+++ compare_data.js
@@ -0,0 +1,154 @@
+var course = [{
+    "id": 0,
+    "progName": "",
+    "entryReq": "",
+    "progOutline": "",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+},{
+    "id": 1,
+    "progName": "Foundation in Computing",
+    "entryReq": "'SPM: 5 Credits, 1 Math Grade B & English Pass', 'O: Level 5 Grade C, 1 Math Grade C & English Grade E', 'UEC: 3 Grade B, 1 Math Grade B & English Grade C'",
+    "progOutline": "'English Language', 'Introduction to Programming', 'Introduction to Computer Networks', 'Information Technology Essentials', 'Mathematics', 'Data Management', 'Web Development', 'Statistics', 'English for Communication', 'Introduction to Multimedia', 'Introduction to Operating Systems', 'Emerging Trends in IT', 'Algebra & Trigonometry', 'Calculus'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 2,
+    "progName": "Diploma in Computer Science",
+    "entryReq": "    'SPM: 3 Credits, Add Math Credit/ Math Credit & AMMS0204 Math Grade C, English Pass', 'O Level: 3 Grade C, Add Math Grade C/ Math Grade C & AMMS0204 Math Grade C, English Grade E', 'UEC: 3 Grade B, 1 Math Grade B, English Grade C'",
+    "progOutline": "'Problem Solving and Programming', 'Computer Architecture', 'Systems Analysis and Design', 'Ethics in Computing', 'Calculus and Algebra', 'Database Development and Applications', 'Software Development Fundamentals', 'Probability and Statistics', 'Fundamentals of Computer Networks', 'Discrete Mathematics', 'Introduction to Interface Design', 'Object-Oriented Programming Techniques', 'Operating Systems', 'Fundamentals of Artificial Intelligence', 'Introduction to Data Structures and Algorithms', 'Systems and Programming Concepts', 'Mobile Application Development', 'Mini Project', 'Parallel and Distributed Computing', 'Introduction to Cybersecurity', 'Industrial Training (20 weeks)'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 3,
+    "progName": "Diploma in Information Technology",
+    "entryReq": "",
+    "progOutline": "'Problem Solving and Programming', 'Introduction to Interface Design', 'Information Technology and Systems', 'Web-Based Integrated Systems', 'Ethics in Computing', 'Web and Mobile Systems', 'Cloud Computing and Business', 'Systems Analysis and Design', 'Object-Oriented Programming Techniques', 'Operating Systems', 'Computer Architecture', 'Fundamentals of Computer Networks', 'Information Management', 'Database Development and Applications', 'Introduction to Cybersecurity', 'Calculus and Algebra', 'Probability and Statistics', 'Discrete Mathematics', 'Discrete Mathematical Structures', 'Industrial Training (20 weeks)'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 4,
+    "progName": "Bachelor of Software Engineering (Honours)",
+    "entryReq": "",
+    "progOutline": "'Problem Solving and Programming', 'Database Management', 'Software Engineering', 'Computer Organisation and Architecture', 'Systems Analysis and Design', 'Operating Systems', 'Object-Oriented Programming', 'Data Structures and Algorithms', 'Fundamentals of Computer Networks', 'Artificial Intelligence', 'Software Testing', 'Software Quality and Measurement', 'Software Requirements Engineering', 'Software Design and Architecture', 'Software Engineering Ethics and Professionalism', 'Software Security and Safety', 'Software Project Management', 'Software Maintenance', 'Collaborative Development', 'Probability and Statistics', 'Discrete Mathematics', 'Project I', 'Project II', 'Industrial Training (6 months)', 'Electives (Choose 6):', 'Formal Methods for Software Engineering', 'Human Computer Interaction', 'Introduction to Internet Security', 'Web-Based Integrated Systems', 'Advanced Discrete Mathematics', 'Advanced Database Management', 'Mobile Application Development', 'Data Science', 'Cloud Computing', 'Agile Software Development', 'Blockchain Application Development', 'Image Processing'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 5,
+    "progName": "Bachelor of Data Science (Honours)",
+    "entryReq": "",
+    "progOutline": "'Problem Solving and Programming', 'Database Management', 'Introduction to Computer Security', 'Probability and Statistics', 'System Analysis and Design', 'Discrete Mathematics, 'Fundamentals of Computer Networks', 'Computer Organisation and Architecture', 'Object-Oriented Programming', 'Operating Systems', 'Statistics for Data Science', 'Software Engineering', 'Data Science', 'Data Visualisation', 'Data Engineering', 'Machine Learning', 'Data Structures and Algorithms', 'Artificial Intelligence', 'Project I', 'Project II', 'Data Warehouse Technology', 'Industrial Training (24 weeks)', 'Elective 1 (Choose 1):', 'Algebra and Calculus', 'Advanced Discrete Mathematics', 'Elective 2 (Choose 1):', 'Natural Language Processing', 'Web-Based Integrated Systems', 'Elective 3 (Choose 1):', 'Internet of Things', 'Graphics Programming', 'Elective 4 (Choose 1):', 'Image Processing', 'Blockchain Application Development', 'Elective 5 (Choose 1):', 'Advanced Database Management', 'Mobile Application Development', 'Elective 6 (Choose 1):', 'Cloud Computing', 'Distributed Systems and Parallel Computing', 'Free Electives (Choose 1):', 'Japanese Language I', 'French Language I', 'Korean Language I'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 6,
+    "progName": "Bachelor of Software Systems Development (Honours)",
+    "entryReq": "",
+    "progOutline": "'IT Fundamentals', 'Systems Analysis and Design', 'Integrative Programming', 'Fundamentals of Computer Networks', 'Human Computer Interaction', 'Cloud Computing', 'Problem Solving and Programming', 'Database Management', 'Object-Oriented Programming', 'Object-Oriented Analysis and Design', 'Data Structures and Algorithms', 'Mobile Application Development', 'Switching and Routing Technologies', 'Enterprise Networking', 'Software Engineering', 'Artificial Intelligence', 'Internet of Things', 'Advanced Database Management', 'Software Project Management', 'Computer Organisation and Architecture', 'Discrete Structures', 'Operating Systems', 'Web-Based Integrated Systems', 'Information Technology Infrastructure', 'Web and Mobile Systems', 'Introduction to Internet Security', 'Software Quality Assurance and Testing', 'Project I', 'Project II', 'Industrial Training (6 months)'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 7,
+    "progName": "Bachelor of Enterprise Information Systems (Honours)",
+    "entryReq": "",
+    "progOutline": "'Problem Solving and Programming', 'Database Management', 'Business and Information Systems', 'Introduction to Computer Security', 'Systems Analysis and Design', 'Fundamentals of Computer Networks', 'Operating Systems', 'Computer Organisation and Architecture', 'Object-Oriented Programming', 'Information Technology Infrastructure', 'Business Process Management', 'Social and Professional Issues', 'Advanced Database Management', 'Web Programming', 'Information Systems Implementation', 'IS Strategy and Management', 'Database Administration', 'Software Project Management', 'Project I', 'Project II', 'Industrial Training (6 months)', 'Specialisation:', 'IT Fundamentals', 'Web Design and Development', 'Enterprise Systems', 'Object-Oriented Analysis and Design', 'Artificial Intelligence', 'Enterprise Resource Planning', 'Data Warehouse Technology', 'Business Intelligence', 'Free Electives (Choose 1):', 'Music Appreciation', 'Principles of Accounting', 'Business Organisation and Management'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 8,
+    "progName": "Bachelor of Information Security (Honours)",
+    "entryReq": "",
+    "progOutline": "'Problem Solving and Programming', 'Database Management', 'Computer Organisation and Architecture', 'IT Fundamentals', 'Systems Analysis and Design', 'Discrete Structures', 'Fundamentals of Computer Networks', 'Operating Systems', 'Object-Oriented Programming', 'Human Computer Interaction', 'Object-Oriented Analysis and Design', 'Switching and Routing Technologies', 'Information Technology Infrastructure', 'Web and Mobile Systems', 'Introduction to Internet Security', 'Advanced Database Management', 'Integrative Programming', 'Cloud Computing', 'Software Project Management', 'Project I', 'Project II', 'Industrial Training (6 months)', 'Web Design and Development', 'Artificial Intelligence', 'Information Assurance and Security', 'Enterprise Networking', 'Software Security', 'Digital Forensics', 'Vulnerability Assessment and Penetration Testing', 'Free Electives (Choose 1):', 'Japanese Language I', 'French Language I', 'Korean Language I'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 9,
+    "progName": "Bachelor of Interactive Software Technology (Honours)",
+    "entryReq": "",
+    "progOutline": "'Problem Solving and Programming', 'Computer Game Studies', 'Database Management', 'Computer Organisation and Architecture', 'Object-Oriented Programming', 'Object-Oriented Analysis and Design', 'Operating Systems', 'Data Structures and Algorithms', 'Software Engineering', 'Graphics Programming', 'Computer Game Programming', 'Social and Professional Issues', 'Artificial Intelligence', 'Fundamentals of Computer Networks', 'Human Computer Interaction', 'Discrete Mathematics', 'Probability and Statistics', 'Mathematics for Games Technology', 'Systems Analysis and Design', '3D Game Development', 'Virtual Reality', 'Computer Games Technology', 'Computer Game Design', 'Software Development Fundamentals', 'Mobile Application Development', 'Parallel and Distributed Computing', 'Information Assurance and Security', 'Project I', 'Project II', 'Industrial Training (6 months)'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 10,
+    "progName": "Bachelor of Management Mathematics with Computing (Honours)",
+    "entryReq": "",
+    "progOutline": "'Discrete Mathematics', 'Calculus I', 'Calculus II', 'Algebra', 'Probability and Statistics', 'Linear Algebra', 'Advanced Calculus', 'Numerical Methods', 'Mathematical and Statistical Software', 'Problem Solving and Programming', 'Database Management', 'Computer Organisation and Architecture', 'Object-Oriented Programming', 'Operating Systems', 'IT Fundamentals', 'Visual Programming', 'Industrial Training (10 weeks)', 'Project', 'Mathematical Statistics', 'Applied Statistics', 'Operations Research I', 'Operations Research II', 'Financial Mathematics', 'Quality Control', 'Group Electives:', 'Microeconomics', 'Macroeconomics'        , 'OR', 'Accounting Methods I', 'Accounting Methods II', 'Electives (Choose 1):', 'Further Applied Statistics', 'Cryptography', 'Advanced Financial Mathematics', 'Electives (Choose 1):', 'Data Science', 'Business Intelligence', 'Cloud Computing', 'Machine Learning'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 11,
+    "progName": "Master of Computer Science",
+    "entryReq": "",
+    "progOutline": "'Research Methodology', 'Master Research', 'Special Topic'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 12,
+    "progName": "Master of Information Technology",
+    "entryReq": "",
+    "progOutline": "'Research Methodology', 'Master Research', 'Special Topic'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 13,
+    "progName": "Master of Science in Mathematical Sciences",
+    "entryReq": "",
+    "progOutline": "'Research Methodology', 'Master Research', 'Special Topic'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 14,
+    "progName": "Doctor of Philosophy (Computer Science)",
+    "entryReq": "",
+    "progOutline": "'Research Methodology', 'Doctoral Research', 'Special Topic'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 15,
+    "progName": "Doctor of Philosophy (Information Technology)",
+    "entryReq": "",
+    "progOutline": "'Research Methodology', 'Doctoral Research', 'Special Topic'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}, {
+    "id": 16,
+    "progName": "Doctor of Philosophy (Mathematical Sciences)",
+    "entryReq": "",
+    "progOutline": "'Research Methodology', 'Doctoral Research', 'Special Topic'",
+    "fees": "",
+    "career": "",
+    "campus": ""
+
+}]
